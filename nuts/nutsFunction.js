ec8ccf32ea0727ab8f4b98e97770476d12c8ade0
Giles Davies <gidavies@github.com>
2023-06-12 14:09:34 +0000

    Update header

--- nuts/nutsFunction.js
+++ nuts/nutsFunction.js
@@ -4,7 +4,7 @@ function displayString() {
     const category = checkedInput.getAttribute('data-category');
     const value = checkedInput.value;
     const result = document.getElementById('result');
-    result.textContent = `It drives me nuts when ${getUniqueString(category, value)}.`;
+    result.textContent = `${getUniqueString(category, value)}.`;
   }
 }
 
@@ -15,40 +15,40 @@ function getUniqueString(category, value) {
         case 'People1':
           return 'I\'m late';
         case 'People2':
-          return 'people stop in doorways';
+          return 'People stop in doorways';
         case 'People3':
-          return 'people are really nosy';
+          return 'People are really nosy';
         default:
           return '';
       }
     case 'driving':
       switch (value) {
         case 'Driving1':
-          return 'see-saw past you on a motorway';
+          return 'Cars see-saw past you on a motorway';
         case 'Driving2':
-          return 'using mixed measurement systems (UK specific)';
+          return 'Using mixed measurement systems (UK specific)';
         case 'Driving3':
-          return 'people pull out slowly from a junction';
+          return 'People pull out slowly from a junction';
         default:
           return '';
       }
     case 'work':
       switch (value) {
         case 'Work1':
-          return 'attending meta meetings';
+          return 'I have meetings about meetings';
         case 'Work2':
-          return 'meetings are poorly scoped';
+          return 'Meetings are poorly scoped';
         case 'Work3':
-          return 'I\'m trying to note down names and roles during introductions';
+          return 'Managing calendar slots with MSFT';
         default:
           return '';
       }
     case 'domestic':
       switch (value) {
         case 'Domestic1':
-          return 'Infinite kitchen bin capacity';
+          return 'The kitchen bin is assumed to have infinite capacity';
         case 'Domestic2':
-          return 'Other people\'s loos';
+          return 'Booking tickets for gigs';
         case 'Domestic3':
           return 'Having to talk to someone to cancel a service';
         default:
@@ -57,20 +57,20 @@ function getUniqueString(category, value) {
     case 'entertainment':
       switch (value) {
         case 'Entertainment1':
-          return 'people who ask what\'s going on in a film, during a film';
+          return 'Someone asks what\'s going on in a film, during a film';
         case 'Entertainment2':
-          return 'pubs don\'t allow dogs';
+          return 'Pubs don\'t allow dogs';
         case 'Entertainment3':
-          return 'restaurants take forever to get more drinks';
+          return 'Restaurants take forever to get more drinks';
         default:
           return '';
       }
     case 'travel':
       switch (value) {
         case 'Travel1':
-          return 'people using phones on speaker';
+          return 'People using phones on speaker';
         case 'Travel2':
-          return 'people walk slowly in front of you';
+          return 'People walk slowly in front of you';
         case 'Travel3':
           return 'I have to move huge numbers of cushions on a bed';
         default:
